ffd1adfcaffd9d6161e8b4428bbd207ba17fda20
Sami Yerunkar <syerunkar@Samis-MacBook-Pro.local>
2025-03-09 14:17:36 -0400

    Revert "Aneesah home page"
    This reverts commit 3a6d48c15cc810e8a41839c8361f86b5991e2564.

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ export default function App() {
       <Router
         root={props => (
           <>
-            {/* <Nav /> */}
+            <Nav />
             <Suspense>{props.children}</Suspense>
           </>
         )}
